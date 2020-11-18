a5e38722d3ca63f0a386c4b5225d815df5b0ae1c
stadvar <stadvar@gmail.com>
2020-11-18 16:51:58 +0200

    made a modal

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,14 @@
-import debounce from "lodash.debounce";
-import { error, success } from "./pnotify";
-import API from "./apiService";
-import LoadMoreBtn from "./loadMoreBtn";
-import onOpenModal from "./openModal";
-import handlebars from "../templates/handlebars.hbs";
-import animateScrollTo from "animated-scroll-to";
-import { refs } from "./refs";
-import { Observer } from "./intersectionObserver";
+import debounce from 'lodash.debounce';
+import { error, success } from './pnotify';
+import API from './apiService';
+import LoadMoreBtn from './loadMoreBtn';
+import onOpenModal from './openModal';
+import onCloseModal from './closeModal';
+import handlebars from '../templates/handlebars.hbs';
+import animateScrollTo from 'animated-scroll-to';
+import refs from './refs';
+import { Observer } from './intersectionObserver';
+
+refs.filmList.addEventListener('click', onOpenModal);
+document.addEventListener('click', onCloseModal);
+window.addEventListener('keydown', onCloseModal);
