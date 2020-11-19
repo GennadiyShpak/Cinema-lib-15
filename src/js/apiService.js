efb65c9653d40e5802bf0861eabf3c9963538d57
stadvar <stadvar@gmail.com>
2020-11-19 19:36:41 +0200

    made modal refresh info

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -2,52 +2,55 @@
 
 const API_KEY = 'bb3f2a9bd6a374d8a5257ae7f0ad6ee7';
 const BASE_URL = 'https://api.themoviedb.org/3/';
-const urlParms = ['trending/movie/week', 'search/movie']
+const urlParms = ['trending/movie/week', 'search/movie'];
 
 export default class MovieService {
-    constructor() {
-        this.page = 1;
-        this.searchQuery = '';
-    }
+  constructor() {
+    this.page = 1;
+    this.searchQuery = '';
+  }
 
-    async fetchMovies () {
-        const url = `${BASE_URL}${urlParms[0]}?api_key=${API_KEY}&page=${this.page}`
-        return this.responceHandler(url)
-    }
+  async fetchMovies() {
+    const url = `${BASE_URL}${urlParms[0]}?api_key=${API_KEY}&page=${this.page}`;
+    return this.responceHandler(url);
+  }
 
-    async searchMovie () {
-        const url = `${BASE_URL}${urlParms[1]}?api_key=${API_KEY}&query=${this.searchQuery}`
-        return this.responceHandler(url)
-    }
+  async searchMovie() {
+    const url = `${BASE_URL}${urlParms[1]}?api_key=${API_KEY}&query=${this.searchQuery}`;
+    return this.responceHandler(url);
+  }
+  async fetchMovieById(id) {
+    //api.themoviedb.org/3/movie/{movie_id}?api_key=<<api_key>>&language=en-US
+    const url = `${BASE_URL}movie/${id}?api_key=${API_KEY}`;
+    return this.responceHandler(url);
+  }
+  async responceHandler(url) {
+    try {
+      const responce = await fetch(url);
 
-    async responceHandler (url) {
-        try{
-            const responce = await fetch(url)
-            
-              if (responce.ok) {
-                  const data = await responce.json();
-                  return data;
-              }
-              throw new Error (data.statusText);
-        }  
-        catch {
-            error=>colsole.log(error)
-        }
+      if (responce.ok) {
+        const data = await responce.json();
+        return data;
+      }
+      throw new Error(data.statusText);
+    } catch {
+      error => colsole.log(error);
     }
+  }
 
-    get query () {
-        return this.searchQuery;
-    }
+  get query() {
+    return this.searchQuery;
+  }
 
-    set query (newSearchQuery) {
-        this.searchQuery=newSearchQuery;
-    }
+  set query(newSearchQuery) {
+    this.searchQuery = newSearchQuery;
+  }
 
-    incrementPage () {
-        this.page += 1;
-    }
+  incrementPage() {
+    this.page += 1;
+  }
 
-    resetInput () {
-        this.page =1;
-    }
-} 
+  resetInput() {
+    this.page = 1;
+  }
+}
--- src/js/handlerModal.js
+++ src/js/handlerModal.js
@@ -1,20 +1,44 @@
+//HANDLER MODAL
 import refs from './refs';
-
+import MovieService from '../js/apiService';
 refs.filmGalery.addEventListener('click', onCardFilmClick);
 refs.modal.addEventListener('click', onBackdropClick);
 
-function onCardFilmClick(e) {
+const getFilmServ = new MovieService();
+const modElems = {
+  img: refs.modal.querySelector('.card-img > img'),
+  title: refs.modal.querySelector('.card-title'),
+  rating: refs.modal.querySelector('.rating-data'),
+  descript: refs.modal.querySelector('.descript-text'),
+};
+
+async function onCardFilmClick(e) {
   if (!e.target.closest('.film__item')) return;
-  //---
   const current = e.target.closest('.film__item');
-  console.log(current.children[0].src);
-  const modalImg = refs.modal.querySelector('.card-img > img');
-  const modalTitle = refs.modal.querySelector('.card-title');
-  modalImg.src = current.children[0].src;
-  modalTitle.textContent = current.children[1].textContent;
+  const data = await getFilmServ.fetchMovieById(current.dataset.id);
+  onUpdateModal(data);
   //---
   refs.modal.classList.remove('is-hidden');
   window.addEventListener('keydown', onEscPress);
+}
+function onUpdateModal({
+  poster_path,
+  title,
+  vote_average,
+  vote_count,
+  popularity,
+  original_title,
+  genres,
+  overview,
+}) {
+  modElems.img.src = `https://image.tmdb.org/t/p/w400${poster_path}`;
+  modElems.title.textContent = title;
+  modElems.rating.children[0].firstElementChild.textContent = vote_average;
+  modElems.rating.children[0].lastElementChild.textContent = vote_count;
+  modElems.rating.children[1].textContent = popularity;
+  modElems.rating.children[2].textContent = original_title;
+  modElems.rating.children[3].textContent = genres[0].name;
+  modElems.descript.textContent = overview;
 }
 
 function onCloseModal() {
@@ -33,3 +57,4 @@ function onBackdropClick(e) {
     onCloseModal();
   }
 }
+//----------------
