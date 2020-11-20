0cfce3f03554a6444ef5d07c909264096ec3edf5
Dmitrij Shamota <su161084sd@gmail.com>
2020-11-20 19:04:36 +0200

    corect style3

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -2,52 +2,52 @@
 
 const API_KEY = 'bb3f2a9bd6a374d8a5257ae7f0ad6ee7';
 const BASE_URL = 'https://api.themoviedb.org/3/';
-const urlParms = ['trending/movie/week', 'search/movie']
+const urlParms = ['trending/movie/week', 'search/movie'];
 
 export default class MovieService {
-    constructor() {
-        this.page = 1;
-        this.searchQuery = '';
-    }
+  constructor() {
+    this.page = 1;
+    this.searchQuery = '';
+  }
 
-    async fetchMovies () {
-        const url = `${BASE_URL}${urlParms[0]}?api_key=${API_KEY}&page=${this.page}`
-        return this.responceHandler(url)
-    }
+  async fetchMovies() {
+    const url = `${BASE_URL}${urlParms[0]}?api_key=${API_KEY}&page=${this.page}`;
+    return this.responceHandler(url);
+  }
 
-    async searchMovie () {
-        const url = `${BASE_URL}${urlParms[1]}?api_key=${API_KEY}&query=${this.searchQuery}`
-        return this.responceHandler(url)
-    }
+  async searchMovie() {
+    const url = `${BASE_URL}${urlParms[1]}?api_key=${API_KEY}&query=${this.searchQuery}`;
+    return this.responceHandler(url);
+  }
 
-    async responceHandler (url) {
-        try{
-            const responce = await fetch(url)
-            
-              if (responce.ok) {
-                  const data = await responce.json();
-                  return data;
-              }
-              throw new Error (data.statusText);
-        }  
-        catch {
-            error=>colsole.log(error)
-        }
-    }
+  async responceHandler(url) {
+    try {
+      const responce = await fetch(url);
+      console.log(responce);
 
-    get query () {
-        return this.searchQuery;
+      if (responce.ok) {
+        const data = await responce.json();
+        return data;
+      }
+      throw new Error(data.statusText);
+    } catch {
+      error => colsole.log(error);
     }
+  }
 
-    set query (newSearchQuery) {
-        this.searchQuery=newSearchQuery;
-    }
+  get query() {
+    return this.searchQuery;
+  }
 
-    incrementPage () {
-        this.page += 1;
-    }
+  set query(newSearchQuery) {
+    this.searchQuery = newSearchQuery;
+  }
 
-    resetInput () {
-        this.page =1;
-    }
-} 
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetInput() {
+    this.page = 1;
+  }
+}
