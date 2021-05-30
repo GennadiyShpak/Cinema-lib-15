525005a94b150f931245790e3eb6218c5b9dba08
dudakvo <48277487+dudakvo@users.noreply.github.com>
2021-05-30 23:09:23 +0300

    Revert "Dev"

--- src/index.js
+++ src/index.js
@@ -7,14 +7,7 @@ import refs from './js/refs';
 import './js/renderMyLibrary';
 import { onSearch, randerGenreFilm } from './js/renderSearchFilm';
 import renderHederHandler from './js/header-logic';
-import {
-  onStudentBtnClick,
-  onBackdropClick,
-  onCloseTeamModal,
-} from './js/teamModal';
-refs.studentBtnClick.addEventListener('click', onStudentBtnClick);
-refs.teamModal.addEventListener('click', onBackdropClick);
-refs.closeBtn.addEventListener('click', onCloseTeamModal);
+
 // randerGenreFilm();
 onLoadPage();
 renderHederHandler();
